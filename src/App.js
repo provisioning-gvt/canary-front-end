a81780f510bc6ebf51a70776328b596bc62736a9
Khi Ann Chua <chua_khi_ann@tech.gov.sg>
2024-06-24 10:20:44 +0800

    chore : fix main form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ProductForm from './ProductForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className="product-form">
       <header className="App-header">
         <h1>(THIS IS PURELY A TEST APP) Product Management</h1>
       </header>
