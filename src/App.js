76a8dc103fe538d5eac1a13f066c648ba3125800
Khi Ann Chua <chua_khi_ann@tech.gov.sg>
2024-06-24 10:23:36 +0800

    chore: fix width

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProductForm from './ProductForm';
 function App() {
   return (
     <div className="product-form">
-      <header className="App-header">
+      <header className="product-form">
         <h1>(THIS IS PURELY A TEST APP) Product Management</h1>
       </header>
       <main>
